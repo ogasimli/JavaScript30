89235de95edd85b8cc137dbec22f250cd9c168d8
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-01-29 16:46:44 +0400

    feat: Solve Challenge #13: Slide in on Scroll

---
+++ 13 - Slide in on Scroll/app.js
@@ -0,0 +1,36 @@
+const sliderImages = document.querySelectorAll('.slide-in');
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+function checkSlide() {
+    sliderImages.forEach(sliderImage => {
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');            
+        }
+    });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
