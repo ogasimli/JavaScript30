b277402039c0d8effd2daa1491d468fa769df8cb
Orkhan Gasimli <orkhan.gasimli@gmail.com>
2018-01-29 13:56:14 +0400

    feat: Solve Challenge #12: Key Sequence Detection

---
+++ 12 - Key Sequence Detection/app.js
@@ -0,0 +1,10 @@
+const pressed = [];
+const secretCode = 'orkhan';
+
+window.addEventListener('keyup', e => {
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    if (pressed.join('').includes(secretCode)) {
+        cornify_add();
+    }
+});
